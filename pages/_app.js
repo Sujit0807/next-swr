2c7e9bbff4456c84152b1fe5389684fcecd188f9
Sujit Mali <malisujit12@gmail.com>
2022-12-03 20:38:00 +0530

    Comment Add

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />      // for adding common part in all components
 }
 
 export default MyApp
